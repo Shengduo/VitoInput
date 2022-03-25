ce743d2eb2b25d568aa0ffd8a29ca485eab8d719
Shengduo <sliu5@caltech.edu>
2022-03-24 22:14:58 -0700

    Changed some naming conventions.

--- UsualSample/RunJobs_varyingB.sh
+++ UsualSample/RunJobs_varyingB.sh
@@ -2,33 +2,36 @@
 cd /home/shengduo/pylith-developer/build/debug/
 source setup.sh
 cd -
-for drs in 8
+for drs in 12 16 24 48
 do
 	for load in 4.75
 	do
-		for vw in 2
+		for Vw3 in 4
 		do
-			for fw in 0.1
+			for fw3 in 0.1 0.2
 			do
 				for theta in 9e-2
 				do
-					for A in 0.020
+					for A in 0.024
 					do
-						for fwW in 0.40
+						for fw2 in 0.1
 						do
-							for VwW in 0.2
+							for Vw2 in 4
 							do
 								for Vw1 in 0.1
 								do
-									for b_final in 0.016 0.024
-									do
-										for b_distance in 2e-6
+									for b_initial in 0.013
+									do 
+										for b_final in 0.011
 										do
-											python3 createInputs_varyingB.py modifyFiles --DRS=$drs --Load=$load --Vw=$vw --VwW=$VwW --Vw1=$Vw1 --fw=$fw --fwW=$fwW --theta=$theta --A=$A --b_final=$b_final --b_distance=$b_distance
-											echo "Running case VaryingB_DRS1_${drs}_A${A}_B${b_final}_${b_distance}_Load${load}_fw${fwW}_${fw}_Vww${Vw1}_${VwW}_${vw}_theta${theta}"
-											pylith --nodes=16 UsualSampleVaryingBVSFH.cfg >& log/VaryingB_DRS1_${drs}_A${A}_B${b_final}_${b_distance}_Load${load}_fw${fwW}_${fw}_Vw${Vw1}_${VwW}_${vw}_theta${theta}.log
-											echo "Finished!"
-											echo
+											for b_distance in 8e-6
+											do
+												python3 createInputs_varyingB.py modifyFiles --DRS=$drs --Load=$load --Vw1=$Vw1 --Vw2=$Vw2 --Vw3=$Vw3 --fw2=$fw2 --fw3=$fw3 --theta=$theta --A=$A --b_initial=$b_initial --b_final=$b_final --b_distance=$b_distance
+												echo "Running case VaryingB_DRS1_${drs}_A${A}_B${b_initial}_${b_final}_${b_distance}_Load${load}_fw${fw2}_${fw3}_Vww${Vw1}_${Vw2}_${Vw3}_theta${theta}"
+												pylith --nodes=16 UsualSampleVaryingBVSFH.cfg >& log/VaryingB_DRS1_${drs}_A${A}_B${b_initial}_${b_final}_${b_distance}_Load${load}_fw${fw2}_${fw3}_Vw${Vw1}_${Vw2}_${Vw3}_theta${theta}.log
+												echo "Finished!"
+												echo
+											done
 										done
 									done
 								done
