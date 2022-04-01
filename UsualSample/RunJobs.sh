f79e5089d6ebd25e80d7afd30262b466aed46d29
Shengduo <sliu5@caltech.edu>
2022-04-01 11:54:33 -0700

    Changed initial traction distribution.

--- UsualSample/RunJobs.sh
+++ UsualSample/RunJobs.sh
@@ -2,7 +2,8 @@
 #cd /home/shengduo/pylith-developer/build/debug/
 #source setup.sh
 #cd /home/shengduo/pylith-developer/build/debug/pylith-nonRegSlipLaw/examples/3d/Vito/VitoInput/UsualSample
-for drs in 2 4 6 8 
+# Fix drs=6 Vw=1.5 fw=0.1 theta=0.036 A=0.016
+for drs in 8
 do
 	for load in 5
 	do
@@ -10,15 +11,18 @@ do
 		do
 			for fw in 0.1
 			do
-				for theta in 0.043 0.042
+				for theta in 0.036
 				do
 					for A in 0.016
 					do	
-						python3 createInputs.py modifyFiles --DRS=$drs --Load=$load --Vw=$Vw --fw=$fw --theta=$theta --A=$A
-						echo "Running case DRS1_${drs}_ModA${A}_Load${load}_fw${fw}_Vw${Vw}_theta${theta}"
-						pylith --nodes=16 UsualSampleVSFH.cfg >& log/DRS1_${drs}_ModA${A}_Load${load}_fw${fw}_Vw${Vw}_theta${theta}.log
-						echo "Finished!"
-						echo
+						for NULoad in 1.5 1
+						do
+							python3 createInputs.py modifyFiles --DRS=$drs --Load=$load --Vw=$Vw --fw=$fw --theta=$theta --A=$A --NULoad=$NULoad
+							echo "Running case DRS1_${drs}_ModA${A}_Load${load}_fw${fw}_Vw${Vw}_theta${theta}_NULoad${NULoad}"
+							pylith --nodes=16 UsualSampleVSFH.cfg >& log/DRS1_${drs}_ModA${A}_Load${load}_fw${fw}_Vw${Vw}_theta${theta}_NULoad${NULoad}.log
+							echo "Finished!"
+							echo
+						done
 					done
 				done
 			done
--- UsualSample/createInputs.py
+++ UsualSample/createInputs.py
@@ -1,19 +1,20 @@
 import fire
 class FileModifier(object):
     """ Modifies the file for further simulation """
-    def modifyFiles(self, DRS, Load, Vw, fw, theta, A):
+    def modifyFiles(self, DRS, Load, Vw, fw, theta, A, NULoad):
         ## Main CFG file
-        fileNamePrefix = "DRS1.5_" + str(DRS) + "ModA" + str(A) + "Load" + str(Load) + "_Vw" + str(Vw) + "_fw" + str(fw) + "_theta" + str(theta)
+        fileNamePrefix = "DRS1.5_" + str(DRS) + "ModA" + str(A) + "Load" + str(Load) + "_Vw" + str(Vw) + "_fw" + str(fw) + "_theta" + str(theta) + "_NULoad2dir" + str(NULoad)
         mainCFGr = open("UsualSampleVSFH.cfg", 'r')
         list_of_lines = mainCFGr.readlines()
         # Change initial theta
         list_of_lines[204] = "friction.db_initial_state.data = [" + str(theta) + "*s]\n"
         # Change output file name
-        list_of_lines[272] = "writer.filename = output/frontsurfFiles/" + fileNamePrefix + "-frontsurf.h5\n"
-        list_of_lines[285] = "writer.filename = output/faultFiles/" + fileNamePrefix + "-fault.h5\n"
+        list_of_lines[262] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-domain.h5\n"
+        list_of_lines[277] = "writer.filename = output/frontsurfFiles/" + fileNamePrefix + "-frontsurf.h5\n"
+        list_of_lines[290] = "writer.filename = output/faultFiles/" + fileNamePrefix + "-fault.h5\n"
 
-        list_of_lines[297] = "writer.filename = output/dumpFiles/upper_crust.h5\n"
-        list_of_lines[308] = "writer.filename = output/dumpFiles/lower_crust.h5\n"
+        list_of_lines[302] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-upper_crust.h5\n"
+        list_of_lines[313] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-lower_crust.h5\n"
 
         mainCFGw = open("UsualSampleVSFH.cfg", 'w')
         mainCFGw.writelines(list_of_lines)
@@ -62,6 +63,28 @@ class FileModifier(object):
         fricDBw.writelines(list_of_lines)
         fricDBr.close()
         fricDBw.close()
+
+        ## Modify NUload
+        NULoadCFGr = open("spatialdb/prescribed_traction_initial.spatialdb", 'r')
+        list_of_lines = NULoadCFGr.readlines()
+        # Change initial prescribed perturbed traction
+        list_of_lines[35] =  " 0.006354  0.003522 -0.005000     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        list_of_lines[36] =  " 0.006354  0.003522  0.005000     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        
+        list_of_lines[38] =  " 0.019473  0.010794 -0.005000     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        list_of_lines[39] =  " 0.019473  0.010794  0.005000     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        
+        list_of_lines[41] =  " 0.021223  0.011764 -0.005000     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        list_of_lines[42] =  " 0.021223  0.011764  0.005000     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        
+        list_of_lines[44] =  " 0.058832  0.032610 -0.005000     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        list_of_lines[45] =  " 0.058832  0.032610  0.005000     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+
+        NULoadCFGw = open("spatialdb/prescribed_traction_initial.spatialdb", 'w')
+        NULoadCFGw.writelines(list_of_lines)
+        # print(fileNamePrefix)
+        NULoadCFGr.close()
+        NULoadCFGw.close()
 
 if __name__ == '__main__':
     fire.Fire(FileModifier)
