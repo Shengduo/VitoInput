e1bc9dc67eea6ce18000aa124c7ad5296ee8147b
Shengduo <sliu5@caltech.edu>
2022-05-16 11:22:46 -0700

    Added another set of parameters.

---
+++ UsualSample/RunJobs_withWall_dir_NP.sh
@@ -0,0 +1,56 @@
+
+# Batch job submission
+#cd /home/shengduo/pylith-developer/build/debug/
+#source setup.sh
+#cd /home/shengduo/pylith-developer/build/debug/pylith-nonRegSlipLaw/examples/3d/Vito/VitoInput/UsualSample
+# Fix drs=6 Vw=1.5 fw=0.1 theta=0.036 A=0.016
+for drs in 1.5
+do
+	for load in 5
+	do
+		for Vw in 0.2
+		do
+			for fw in 0.33
+			do
+				for theta1 in 0.036
+				do
+					for theta2 in 0.036
+					do
+						for A in 0.011
+						do	
+							for AmB in -0.005
+							do
+								if [[ $(echo "$A<=$AmB" |bc -l) -eq 1 ]]; then
+									echo "Shit!"
+									continue
+								fi
+								for NULoad in 0
+								do
+									for meshFineness in 1
+									do
+										for duration in 120
+										do
+											# Start the timer
+											start_time=$(date +%s)
+											
+											fileNamePrefix=${meshFineness}NPDirWithWallDRS1.5_${drs}ModA_${A}_AmB${AmB}_Load${Load}_Vw${Vw}_fw${fw}_theta${theta1}_${theta1}_NULoad2dir${NULoad}_duration${duration}
+											python3 createInputs_withWall_Dirichlet_NP.py modifyFiles --DRS=$drs --Load=$load --Vw=$Vw --fw=$fw --theta1=$theta1 --theta2=$theta1 --A=$A --AmB=$AmB --NULoad=$NULoad --meshFineness=$meshFineness --duration=$duration
+											echo "Running case ${fileNamePrefix}"
+											pylith --nodes=16 UsualSampleVSFH_withWall_Dirichlet.cfg >& log/${fileNamePrefix}.log
+
+											# End the timer, report time
+											end_time=$(date +%s)
+											elapsed=$(( end_time - start_time ))
+											echo "Finished in ${elapsed} s!"
+											echo
+										done
+									done
+								done
+							done
+						done
+					done
+				done
+			done
+		done
+	done
+done
---
+++ UsualSample/createInputs_withWall_Dirichlet_NP.py
@@ -0,0 +1,323 @@
+import fire
+import math
+class FileModifier(object):
+    # Define the size of wall
+    WallPos1 = " -0.005000"
+    WallPos2 = " -0.004100"
+    WallPos3 = " -0.003900"
+    WallPos4 = "  0.004300"
+    WallPos5 = "  0.004500"
+    WallPos6 = "  0.005000"
+
+    """ Modifies the file for further simulation """
+    def modifyFiles(self, DRS, Load, Vw, fw, theta1, theta2, A, AmB, NULoad, meshFineness, duration):
+        ## If meshFineness is 2
+        if meshFineness == 2:
+            backgroundCFGr = open("pylithapp.cfg", 'r')
+            list_of_lines = backgroundCFGr.readlines()
+
+            # Change the input mesh file to the finer one
+            list_of_lines[46] = "filename = mesh/finer_tet.exo\n"
+            backgroundCFGw = open("pylithapp.cfg", 'w')
+            backgroundCFGw.writelines(list_of_lines)
+
+            # close files
+            backgroundCFGr.close()
+            backgroundCFGw.close()
+        
+        elif meshFineness == 3:
+            backgroundCFGr = open("pylithapp.cfg", 'r')
+            list_of_lines = backgroundCFGr.readlines()
+
+            # Change the input mesh file to the finer one
+            list_of_lines[46] = "filename = mesh/finer_tet3.exo\n"
+            backgroundCFGw = open("pylithapp.cfg", 'w')
+            backgroundCFGw.writelines(list_of_lines)
+
+            # close files
+            backgroundCFGr.close()
+            backgroundCFGw.close()
+
+        else:
+            backgroundCFGr = open("pylithapp.cfg", 'r')
+            list_of_lines = backgroundCFGr.readlines()
+
+            # Change the input mesh file to the finer one
+            list_of_lines[46] = "filename = mesh/myGeometryTetFiner.exo\n"
+            backgroundCFGw = open("pylithapp.cfg", 'w')
+            backgroundCFGw.writelines(list_of_lines)
+
+            # close files
+            backgroundCFGr.close()
+            backgroundCFGw.close()
+
+
+        ## Main CFG file
+        fileNamePrefix = str(meshFineness) + "NPDirWithWallDRS1.5_" + str(DRS) + "ModA" + str(A) + "AmB" + str(AmB) + "Load" + str(Load) + "_Vw" + str(Vw) + "_fw" + str(fw) + "_theta" + str(theta1) + "_" + str(theta2) + "_NULoad2dir" + str(NULoad) + "_duration" + str(duration)
+        mainCFGr = open("UsualSampleVSFH_withWall_Dirichlet.cfg", 'r')
+        list_of_lines = mainCFGr.readlines()
+        
+        # First deal with theta2, assuming alpha = 29 \deg, tan(alpha) = 0.554309051452769, f_* = 0.58, linear slip rate = 1e-9
+        B = A - AmB
+        if theta2 < 0.0:
+            # Apply V_ini = 10^{\theta_2};
+            f = math.tan(29 / 180 * math.pi)
+            Vlin = 1.0e-9
+            Vini = 10.0**theta2
+            if theta2 < - 9:
+                temp = f - 0.58 - A * math.log(Vlin / 1e-6) + A * (1 - Vini / Vlin)
+                theta2 = math.exp(temp / B) * DRS / 1e-6 
+            else:
+                temp = f - 0.58 - A * math.log(Vini / 1e-6)
+                theta2 = math.exp(temp / B) * DRS / 1e-6 
+        print("theta2 =", theta2)
+
+        # Change friction file
+        list_of_lines[197] = "friction.db_properties.iohandler.filename = spatialdb/rateStateProps_withWall_NP.spatialdb\n"
+        
+        # Change the duration of simulation time
+        list_of_lines[77] = "total_time = " + str(duration / 1.0e6) + "*s\n"
+        # Change output file name
+        list_of_lines[262] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-domain.h5\n"
+        list_of_lines[277] = "writer.filename = output/frontsurfFiles/" + fileNamePrefix + "-frontsurf.h5\n"
+        list_of_lines[290] = "writer.filename = output/faultFiles/" + fileNamePrefix + "-fault.h5\n"
+
+        list_of_lines[302] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-upper_crust.h5\n"
+        list_of_lines[313] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-lower_crust.h5\n"
+
+        mainCFGw = open("UsualSampleVSFH_withWall_Dirichlet.cfg", 'w')
+        mainCFGw.writelines(list_of_lines)
+        # print(fileNamePrefix)
+        mainCFGr.close()
+        mainCFGw.close()
+
+        ## Load .timedb file
+        timeDBr = open("spatialdb/perturbation_cycle.timedb", 'r')
+        list_of_lines = timeDBr.readlines()
+        # print(list_of_lines)
+        # Change load magnitude
+        list_of_lines[15] = "0.1e-4 " + str(Load / 10.) + "\n"
+        list_of_lines[16] = "0.14e-4 " + str(Load / 10.) + "\n"
+        list_of_lines[17] = "0.16e-4 " + str(0.0) + "\n"
+        timeDBw = open("spatialdb/perturbation_cycle.timedb", 'w')
+        timeDBw.writelines(list_of_lines)
+        timeDBr.close()
+        timeDBw.close()
+
+        ## Modify rateStateProps
+        fricDBr = open("spatialdb/rateStateProps_withWall_NP.spatialdb", 'r')
+        list_of_lines = fricDBr.readlines()
+        
+        list_of_lines[50] = " 0.006354  0.003522 -0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  " + str(B) + "  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[51] = " 0.006354  0.003522  0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  " + str(B) + "  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[59] = " 0.063204  0.035034 -0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  " + str(B) + "  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[60] = " 0.063204  0.035034  0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  " + str(B) + "  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        
+        # Modify the locations
+        Wallposition = [self.WallPos3, self.WallPos4, self.WallPos2, self.WallPos5, self.WallPos1, self.WallPos6]
+        counter = 0;
+        for index in range(32, 85):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+        
+        # Write files
+        fricDBw = open("spatialdb/rateStateProps_withWall_NP.spatialdb", 'w')
+        fricDBw.writelines(list_of_lines)
+        fricDBr.close()
+        fricDBw.close()
+
+        ## Modify NUload
+        NULoadCFGr = open("spatialdb/prescribed_traction_initial_withWall.spatialdb", 'r')
+        list_of_lines = NULoadCFGr.readlines()
+
+        # First set all loads to be zero
+        for index in range(29, 100):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:29] + "     " + "0.0" + "  0.0  " + "0.0" + "\n"
+                
+
+        # Change initial prescribed perturbed traction
+        list_of_lines[53] =  " 0.006354  0.003522 -0.003800     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        list_of_lines[54] =  " 0.006354  0.003522  0.003800     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        
+        list_of_lines[62] =  " 0.019473  0.010794 -0.003800     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        list_of_lines[63] =  " 0.019473  0.010794  0.003800     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+        
+        list_of_lines[71] =  " 0.021223  0.011764 -0.003800     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        list_of_lines[72] =  " 0.021223  0.011764  0.003800     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        
+        list_of_lines[80] =  " 0.063204  0.035034 -0.003800     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+        list_of_lines[81] =  " 0.063204  0.035034  0.003800     " + str(-NULoad * 0.554309051452769 / 3.) + "  0.0  " + str(-NULoad / 3.) + "\n"
+
+        # Modify the locations
+        Wallposition = [self.WallPos1, self.WallPos6, self.WallPos2, self.WallPos5, self.WallPos3, self.WallPos4]
+        counter = 0;
+        for index in range(29, 100):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+
+        # Write files
+        NULoadCFGw = open("spatialdb/prescribed_traction_initial_withWall.spatialdb", 'w')
+        NULoadCFGw.writelines(list_of_lines)
+        # print(fileNamePrefix)
+        NULoadCFGr.close()
+        NULoadCFGw.close()
+
+        ## Modify state variable
+        THETAr = open("spatialdb/stateVariable_withWall.spatialdb")
+        list_of_lines = THETAr.readlines()
+
+        # Modify the locations
+        Wallposition = [self.WallPos1, self.WallPos6, self.WallPos2, self.WallPos5, self.WallPos3, self.WallPos4]
+        counter = 0;
+        for index in range(27, 80):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+
+        # Change somelines into theta1, some into theta2
+        set_of_theta2_lines = [" 0.006354  0.003522" + self.WallPos3, 
+                               " 0.006354  0.003522" + self.WallPos4, 
+                               " 0.063204  0.035034" + self.WallPos3, 
+                               " 0.063204  0.035034" + self.WallPos4]
+        for index in range(27, 80):
+            if len(list_of_lines[index]) > 29:
+                if list_of_lines[index][0:29] in set_of_theta2_lines:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(theta2) + "\n"
+                else:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(theta1) + "\n"
+        
+
+        THETAw = open("spatialdb/stateVariable_withWall.spatialdb", 'w')
+        THETAw.writelines(list_of_lines)
+        THETAr.close()
+        THETAw.close()
+
+    """ Modifies the file for further simulation """
+    def modifyFiles_GougeDifferentLoad(self, DRS, Load, Vw, fw, theta1, theta2, A, NULoad):
+        
+
+        ## Main CFG file
+        fileNamePrefix = "1DiffNULoadWithWallDRS1.5_" + str(DRS) + "ModA" + str(A) + "Load" + str(Load) + "_Vw" + str(Vw) + "_fw" + str(fw) + "_theta" + str(theta1) + "_" + str(theta2) + "_NULoad2dir" + str(NULoad)
+        mainCFGr = open("UsualSampleVSFH_withWall.cfg", 'r')
+        list_of_lines = mainCFGr.readlines()
+
+        # Change output file name
+        list_of_lines[262] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-domain.h5\n"
+        list_of_lines[277] = "writer.filename = output/frontsurfFiles/" + fileNamePrefix + "-frontsurf.h5\n"
+        list_of_lines[290] = "writer.filename = output/faultFiles/" + fileNamePrefix + "-fault.h5\n"
+
+        list_of_lines[302] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-upper_crust.h5\n"
+        list_of_lines[313] = "writer.filename = output/dumpFiles/" + fileNamePrefix + "-lower_crust.h5\n"
+
+        mainCFGw = open("UsualSampleVSFH_withWall.cfg", 'w')
+        mainCFGw.writelines(list_of_lines)
+        # print(fileNamePrefix)
+        mainCFGr.close()
+        mainCFGw.close()
+
+        ## Load .timedb file
+        timeDBr = open("spatialdb/perturbation_cycle.timedb", 'r')
+        list_of_lines = timeDBr.readlines()
+
+        # Change load magnitude
+        list_of_lines[15] = "0.1e-4 " + str(Load / 10.) + "\n"
+        list_of_lines[16] = "0.14e-4 " + str(Load / 10.) + "\n"
+        list_of_lines[17] = "0.16e-4 " + str(0.0) + "\n"
+        timeDBw = open("spatialdb/perturbation_cycle.timedb", 'w')
+        timeDBw.writelines(list_of_lines)
+        timeDBr.close()
+        timeDBw.close()
+
+        ## Modify rateStateProps
+        fricDBr = open("spatialdb/rateStateProps_withWall.spatialdb", 'r')
+        list_of_lines = fricDBr.readlines()
+
+        list_of_lines[50] = " 0.006354  0.003522 -0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  0.003  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[51] = " 0.006354  0.003522  0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  0.003  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[59] = " 0.063204  0.035034 -0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  0.003  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+        list_of_lines[60] = " 0.063204  0.035034  0.003800     0.58  1e-6   " + str(float(DRS) * 1e-6)+ "  " + str(A) + "  0.003  0.0  " + str(fw) + "  " + str(Vw) + "\n"
+
+        # Modify the locations
+        Wallposition = [self.WallPos3, self.WallPos4, self.WallPos2, self.WallPos5, self.WallPos1, self.WallPos6]
+        counter = 0;
+        for index in range(32, 85):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+        
+        fricDBw = open("spatialdb/rateStateProps_withWall.spatialdb", 'w')
+        fricDBw.writelines(list_of_lines)
+        fricDBr.close()
+        fricDBw.close()
+
+        ## Modify NUload
+        NULoadCFGr = open("spatialdb/prescribed_traction_initial_withWall.spatialdb", 'r')
+        set_of_NULoad_lines = [" 0.006354  0.003522" + self.WallPos3, 
+                               " 0.006354  0.003522" + self.WallPos4, 
+                               " 0.063204  0.035034" + self.WallPos3, 
+                               " 0.063204  0.035034" + self.WallPos4, 
+                               " 0.019473  0.010794" + self.WallPos3, 
+                               " 0.019473  0.010794" + self.WallPos4, 
+                               " 0.021223  0.011764" + self.WallPos3, 
+                               " 0.021223  0.011764" + self.WallPos4]
+                            
+        list_of_lines = NULoadCFGr.readlines()
+        ratio = 0.2 * 0.01 / (0.0078 * (0.058832 - 0.006354))
+
+        # Modify the locations
+        Wallposition = [self.WallPos1, self.WallPos6, self.WallPos2, self.WallPos5, self.WallPos3, self.WallPos4]
+        counter = 0;
+        for index in range(29, 100):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+        
+        # Change load
+        for index in range(29, 100):
+            if len(list_of_lines[index]) > 29:
+                if list_of_lines[index][0:29] in set_of_NULoad_lines:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(NULoad * 0.554309051452769) + "  0.0  " + str(NULoad) + "\n"
+                else:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(-NULoad * 0.554309051452769 / ratio) + "  0.0  " + str(-NULoad / ratio) + "\n"
+
+        NULoadCFGw = open("spatialdb/prescribed_traction_initial_withWall.spatialdb", 'w')
+        NULoadCFGw.writelines(list_of_lines)
+        # print(fileNamePrefix)
+        NULoadCFGr.close()
+        NULoadCFGw.close()
+
+        ## Modify state variable
+        THETAr = open("spatialdb/stateVariable_withWall.spatialdb")
+        list_of_lines = THETAr.readlines()
+
+        # Modify the locations
+        Wallposition = [self.WallPos1, self.WallPos6, self.WallPos2, self.WallPos5, self.WallPos3, self.WallPos4]
+        counter = 0;
+        for index in range(27, 80):
+            if len(list_of_lines[index]) > 29:
+                list_of_lines[index] = list_of_lines[index][0:19] + Wallposition[counter % 6] + list_of_lines[index][29:]
+                counter = counter + 1
+        
+        # Change somelines into theta1, some into theta2
+        set_of_theta2_lines = [" 0.006354  0.003522" + self.WallPos3, 
+                               " 0.006354  0.003522" + self.WallPos4, 
+                               " 0.063204  0.035034" + self.WallPos3, 
+                               " 0.063204  0.035034" + self.WallPos4]
+        
+        for index in range(27, 80):
+            if len(list_of_lines[index]) > 29:
+                if list_of_lines[index][0:29] in set_of_theta2_lines:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(theta2) + "\n"
+                else:
+                    list_of_lines[index] = list_of_lines[index][0:29] + "     " + str(theta1) + "\n"
+        
+        THETAw = open("spatialdb/stateVariable_withWall.spatialdb", 'w')
+        THETAw.writelines(list_of_lines)
+        THETAr.close()
+        THETAw.close()
+
+if __name__ == '__main__':
+    fire.Fire(FileModifier)
