6380c725d94332a7282d6edfe928b864d70df0a4
Shengduo <sliu5@caltech.edu>
2022-05-26 11:44:18 -0700

    Changed prestress formulation.

--- UsualSample/RunJobs_withWall_dir_NP.sh
+++ UsualSample/RunJobs_withWall_dir_NP.sh
@@ -4,27 +4,27 @@
 #source setup.sh
 #cd /home/shengduo/pylith-developer/build/debug/pylith-nonRegSlipLaw/examples/3d/Vito/VitoInput/UsualSample
 # Fix drs=6 Vw=1.5 fw=0.1 theta=0.036 A=0.016
-for drs in 1.5
+for drs in 1.5 3
 do
 	for load in 5
 	do
-		for Vw in 0.2
+		for Vw in 2
 		do
-			for fw in 0.33
+			for fw in 0.1
 			do
-				for theta1 in 0.036
+				for theta1 in 0.00224
 				do
-					for theta2 in 0.036
+					for theta2 in -9
 					do
 						for A in 0.011
 						do	
-							for AmB in -0.005
+							for AmB in 0.005
 							do
 								if [[ $(echo "$A<=$AmB" |bc -l) -eq 1 ]]; then
 									echo "Shit!"
 									continue
 								fi
-								for NULoad in 0
+								for NULoad in 1 2 0
 								do
 									for meshFineness in 1
 									do
@@ -33,8 +33,8 @@ do
 											# Start the timer
 											start_time=$(date +%s)
 											
-											fileNamePrefix=${meshFineness}NPDirWithWallDRS1.5_${drs}ModA_${A}_AmB${AmB}_Load${Load}_Vw${Vw}_fw${fw}_theta${theta1}_${theta1}_NULoad2dir${NULoad}_duration${duration}
-											python3 createInputs_withWall_Dirichlet_NP.py modifyFiles --DRS=$drs --Load=$load --Vw=$Vw --fw=$fw --theta1=$theta1 --theta2=$theta1 --A=$A --AmB=$AmB --NULoad=$NULoad --meshFineness=$meshFineness --duration=$duration
+											fileNamePrefix=${meshFineness}NPDirWithWallDRS1.5_${drs}ModA_${A}_AmB${AmB}_Load${load}_Vw${Vw}_fw${fw}_theta${theta1}_${theta2}_NULoad2dir${NULoad}_duration${duration}
+											python3 createInputs_withWall_Dirichlet_NP_newload.py modifyFiles --DRS=$drs --Load=$load --Vw=$Vw --fw=$fw --theta1=$theta1 --theta2=$theta2 --A=$A --AmB=$AmB --NULoad=$NULoad --meshFineness=$meshFineness --duration=$duration
 											echo "Running case ${fileNamePrefix}"
 											pylith --nodes=16 UsualSampleVSFH_withWall_Dirichlet.cfg >& log/${fileNamePrefix}.log
 
